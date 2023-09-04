8ae8a8df94c779fe86791cc189e4cada8bb6f2d2
FanidiyaTasya <fanidiyatasya@gmail.com>
2023-09-04 08:25:53 +0700

    main

---
+++ Acara2/variable.php
@@ -0,0 +1,9 @@
+<?php
+$nim = "E41222742";
+$nama = "Tasya ";
+$prodi = "Teknik Informatika";
+echo "Selamat Praktikum ";
+echo $nim . " ";
+echo $nama . " ";
+echo $prodi;
+?>
--- folder/variable.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nim="E41222742 ";
-$nama="Tasya ";
-$prodi="Teknik Informatika ";
-echo "Selamat Praktikum";
-echo $nim;
-echo $nama;
-
