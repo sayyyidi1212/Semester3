3ba69ed8f2b79e5fe0e3419fb185e0915fa309a8
FanidiyaTasya <fanidiyatasya@gmail.com>
2023-09-07 18:25:48 +0700

    main

--- Acara2/if_else.php
+++ Acara2/if_else.php
@@ -1,4 +1,16 @@
 <?php
-if (true)
-echo "Selamat datang di Politeknik Negeri Jember";
+$username = "Tasya";
+$password = "1234";
+
+echo "Masukkan Username: ";
+$username = trim(fgets(STDIN));
+
+echo "Masukkan Password: ";
+$password = trim(fgets(STDIN));
+
+if ($username === $username && $password === $password) {
+    echo "Login berhasil!\n";
+} else {
+    echo "Login gagal! Silakan coba lagi.\n";
+}
 ?>
